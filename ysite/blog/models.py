813841b66b321f8611b94d6eec7d65ba3d1f63b4
esparzar <esparzar535@gmail.com>
2019-07-06 08:53:03 +0100

    added bootstrap

--- ysite/blog/models.py
+++ ysite/blog/models.py
@@ -4,17 +4,17 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
 	#blog properties
-		author = models.ForeignKey('auth.User')
-		title = models.CharField(max_length=200)
-		published_date = models.DateTimeField(blank=True, null=True)
-		created_date = models.DateTimeField(default=timezone.now)
-		text = models.TextField()
+	author = models.ForeignKey('auth.User')
+	tittle = models.CharField(max_length=200)
+	published_date = models.DateTimeField(blank=True, null=True)
+	created_date = models.DateTimeField(default=timezone.now)
+	text = models.TextField()
 
-def publish(self):
-	self.published_date = timezone.now()
-	self.save()
+	def poblisheme(self):
+		self.published_date = timezone.now()
+		self.save()
 
-def __str__(self):
-	return self.title
+	def __str__(self):
+		return self.tittle
 
 
